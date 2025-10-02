64db3e9a95756f893e2d3374b0952e7a6902a72c
MadhanKumar1528 <gmadhankumar121@gmail.com>
2025-10-02 15:51:56 +0530

    Create employeemodel.js

---
+++ employeemodel.js
@@ -0,0 +1,18 @@
+const db = require("../config/db");
+
+const Employee = {
+  getAll: (callback) => {
+    db.query("SELECT * FROM employees", callback);
+  },
+  create: (data, callback) => {
+    db.query("INSERT INTO employees SET ?", data, callback);
+  },
+  update: (id, data, callback) => {
+    db.query("UPDATE employees SET ? WHERE id=?", [data, id], callback);
+  },
+  delete: (id, callback) => {
+    db.query("DELETE FROM employees WHERE id=?", [id], callback);
+  }
+};
+
+module.exports = Employee;
